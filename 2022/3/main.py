6c2fa0487a0845b7089e65b3824a4cdef5687a3c
ponterik <pontus.z.eriksson@outlook.com>
2022-12-04 20:24:04 +0100

    day 3 part 2

--- 2022/3/main.py
+++ 2022/3/main.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import io
 
 def get_input():
@@ -15,6 +16,15 @@ def compareLists(l1, l2):
                 items.add(x)
     return items
 
+
+
+def generateDict(list):
+    dic = defaultdict(int)
+    for x in list:
+        dic[x] += 1
+    return dic
+
+
 def getPriorityScore(items):
     score = 0
     for x in items:
@@ -23,6 +33,25 @@ def getPriorityScore(items):
         else:
             score = score + ord(x) -38
     return score
+
+def compareLists3(l1, l2, l3):
+    score = 0
+    d1 = generateDict(l1)
+    d2 = generateDict(l2)
+    d3 = generateDict(l3)
+    for x1 in d1:
+        for x2 in d2:
+            if(x1 == x2):
+                for x3 in d3:
+                    if(x1 == x3):
+                        if(x1.islower()):
+                            score = score + ord(x1) - 96
+                        else:
+                            score = score + ord(x1) -38
+    return score
+
+
+
 total = 0
 for line in lines:
     midwayPoint = int(len(line)/2)
@@ -30,3 +59,23 @@ for line in lines:
     l2 = line[-midwayPoint:]
     total = total + getPriorityScore(compareLists(l1, l2))
 print(total)
+
+
+
+def batchLists(items):
+    batchedList = []
+    tempList = []
+    for i in range(1, len(items) + 1):
+        tempList.append(items[i-1])
+        if(i%3 == 0):
+            print(i)
+            batchedList.append((tempList[0], tempList[1], tempList[2]))
+            tempList = []
+    print("batchedlist: ", len(batchedList))
+    return batchedList
+tot = 0
+for batch in batchLists(lines):
+    print("__________")
+    print(batch)
+    tot = tot + compareLists3(batch[0], batch[1], batch[2])
+print(tot)
